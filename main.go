515146e136cfbaf2292f9d1f8643f62c21157b7c
EndyPratama <endypratama2999@gmail.com>
2022-11-17 13:00:07 +0700

    update procfile

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
 
 func main() {
-	log.Fatal("Salah masuk file gan")
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", greet)
+	log.Print("listening on :" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
